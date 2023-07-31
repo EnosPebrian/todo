73a3d2f21a3b747dc264c8f4ad2563d992232493
enospebrian1 <54843640+enospebrian1@users.noreply.github.com>
2023-07-31 10:04:31 +0700

    update dashboard function add pages

--- src/App.jsx
+++ src/App.jsx
@@ -4,14 +4,22 @@ import Body from "./components/Body";
 import "./components/style.css";
 import Head from "./components/Head";
 import Time from "./components/Time";
+import { Route, Routes } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
 
 function App() {
+  const Account = [
+    { name: "User1", password: "pass", email: "user1@mail.com" },
+  ];
+
   return (
-    <section>
-      <Head></Head>
-      <Time></Time>
-      <Body></Body>
-    </section>
+    <>
+      <Routes>
+        {/* <Route path="login" element={<Login />}></Route>
+        <Route path="register" element={<Register />} /> */}
+        <Route path="dashboard" element={<Dashboard />} />
+      </Routes>
+    </>
   );
 }
 
---
+++ src/pages/Dashboard.jsx
@@ -0,0 +1,15 @@
+import Body from "../components/Body";
+import Head from "../components/Head";
+import Time from "../components/Time";
+
+function Dashboard() {
+  return (
+    <section className="dashboard">
+      <Head></Head>
+      <Time />
+      <Body />
+    </section>
+  );
+}
+
+export default Dashboard;
